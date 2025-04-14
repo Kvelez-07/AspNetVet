0052a57bb88bf6c36d6c55be4f5433e27bcfbfb4
Maicol Vargas <maikvargas04@gmail.com>
2025-04-14 14:07:15 -0600

    CRUD Usuarios

---
+++ AspNetVet/Controllers/UsersController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class UsersController : Controller
+{
+    public ActionResult Index()
+    {
+        return View();
+    }
+
+    public ActionResult AddUser()
+    {
+        return View();
+    }
+    public ActionResult EditUser()
+    {
+        return View();
+    }
+}
---
+++ AspNetVet/Models/Users.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace AspNetVet.Models
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Correo { get; set; }
+        public string Telefono { get; set; }
+        public string Cargo { get; set; }
+    }
+}
